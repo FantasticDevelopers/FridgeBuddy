c4972fd94d3fb3e7c20d0544f72da9cb9d1f40db
Amandeep Singh Walia <deepamanwalia@gmail.com>
2022-07-20 20:38:47 -0400

    Initial Commit

---
+++ FridgeBuddy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FridgeBuddy
+//
+//  Created by Amandeep on 2022-07-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FridgeBuddy/FridgeBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridgeBuddyApp.swift
+//  FridgeBuddy
+//
+//  Created by Amandeep on 2022-07-20.
+//
+
+import SwiftUI
+
+@main
+struct FridgeBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
